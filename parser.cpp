00c9ad04d27c3dd2112372149b0fd40325c1c8fd
anferoar96 <afromana@unal.edu.co>
2020-05-09 18:46:18 -0500

    Gramatica structure

--- parser.cpp
+++ parser.cpp
@@ -1,9 +1,27 @@
 #include "parser.h"
 #include <iostream>
 #include <string>
+#include <map>
+#include <vector>
+
 
 using namespace std;
 
+typedef multimap<string, vector<string>> Map;
+
+
+
 string parser_func(vector<Token> analisis){
+
+    Map map;
+    map.insert(Map::value_type("TEST", {"HOLA","2","3"}));
+    map.insert(Map::value_type("TEST", {"HOLA2","4","5"}));
+
+    for (auto itr = map.begin(); itr != map.end(); itr++) {
+       for (int i=0;i<itr->second.size();i++){
+           cout<<itr->second[i]<<" ";
+       }
+       cout<<endl;
+    }
     return to_string(analisis.size());
 }
