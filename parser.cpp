46beffa43f939e7892ae4b57512e7daaf59da4d4
anferoar96 <afromana@unal.edu.co>
2020-05-09 22:51:32 -0500

    Changes in grammar representation

--- parser.cpp
+++ parser.cpp
@@ -3,26 +3,57 @@
 #include <string>
 #include <map>
 #include <vector>
-
+#include <stack>
+#include <algorithm>
 
 using namespace std;
 
 typedef multimap<string, vector<vector<string>>> Map;
 
+
+Map gramatica={
+    {Map::value_type("inicial", {{"var_def"},{"func_def"},{"class_def"},{"stmt"},{"multi_init"}})},
+    {Map::value_type("var_def",{{"typed_var","TK_ASIG","literal","NEWLINE"}})},
+    {Map::value_type("literal",{{"NONE"},{"TRUE"},{"FALSE"},{"TK_ENTERO"}})},
+    {Map::value_type("typed_var", {{"id_n","TK_DOS_PUNTOS","type"},{"multi_var"}})},
+    {Map::value_type("type", {{"id_n"},{"tipo_dato"}})},
+    {Map::value_type("tipo_dato", {{"INT"},{"STRING"}})},
+    {Map::value_type("id_n", {{"ID"}})}
+};
+
 string parser_func(vector<Token> analisis){
 
-    Map map;
-    map.insert(Map::value_type("inicial", {{"var_def"},{"func_def"},{"class_def"},{"stmt"},{"multi_init"}}));
-    map.insert(Map::value_type("var_def",{{"typed_var","TK_ASIG","literal","NEWLINE"}}));
-    map.insert(Map::value_type("literal",{{"NONE"},{"TRUE"},{"FALSE"},{"TK_ENTERO"}}));
-    map.insert(Map::value_type("typed_var", {{"id_n","TK_DOS_PUNTOS","type"},{"multi_var"},{"tipo_dato","TK_DOS_PUNTOS","type"}}));
-    map.insert(Map::value_type("id_n", {{"ID"}}));
+    stack<int> posicion;
+    stack<vector<string>> estado;
 
-    for (auto itr = map.begin(); itr != map.end(); itr++) {
-       for (int i=0;i<itr->second.size();i++){
-           cout<<itr->second[i][0]<<" ";
-       }
-       cout<<endl;
-    }
+
+    string inicial="inicial";
+    auto word =gramatica.find(inicial);
+    auto vec = word->second[0];
+    cout<<vec[0]<<endl;
+    
+   /* while(true){
+        int contvec=0;
+        auto word=gramatica.find(inicial);
+        auto vec=word->second[0];
+        for(int j=0;j<vec.size();j++){
+            if(isupper(vec[0][0])){
+                string res=analisis[contvec].id;
+                transform(vec[0].begin(), vec[0].end(), vec[0].begin(), ::tolower);
+                if(res.compare(vec[0])!=0){
+                    cout<<"Error semantico"<<endl;
+                }
+                contvec++;
+            }else{
+                posicion.push(contvec);
+                estado.push(word->second[0]);
+                inicial=j;
+                break;
+            }
+        }
+
+    } */
+    
+
     return to_string(analisis.size());
 }
